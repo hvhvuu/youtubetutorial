28924e20b4d9e414b6bed589e33668a502ac1d25
hvhvuu <ishaan.venkat@gmail.com>
2020-05-23 20:25:31 -0700

    Update ytTutorial.py

--- ytTutorial.py
+++ ytTutorial.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='!')
-TOKEN = 'TOKEN_Goes_Here'
+TOKEN = 'NzEzODI1NzU1MzY0MzI3NDY1.XslwFg.CLHrEoBACY2ktSGschkrnYQnChM'
 
 
 @client.event
@@ -11,5 +11,19 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+
+@client.event
+async def on_member_join(member):
+    for channel in member.guild.channels:
+        if str(channel) == 'member-log':
+            await channel.send(f'Welcome to the server{member.mention}')
+
+
+@client.event
+async def on_member_remove(member):
+    for channel in member.guild.channels:
+        if str(channel) == 'member-log':
+            await channel.send(f'Bye Bye {member.mention}')
 
 client.run(TOKEN)
